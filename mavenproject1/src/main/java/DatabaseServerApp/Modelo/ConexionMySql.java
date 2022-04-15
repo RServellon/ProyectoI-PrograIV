bd583cc6b48d167440d25d1a73448a4d6fb41204
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2022-04-15 14:29:23 -0600

    quitando implementacion no funcional

--- mavenproject1/src/main/java/DatabaseServerApp/Modelo/ConexionMySql.java
+++ mavenproject1/src/main/java/DatabaseServerApp/Modelo/ConexionMySql.java
@@ -4,65 +4,12 @@
  */
 package DatabaseServerApp.Modelo;
 
-import com.mysql.jdbc.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /**
  *
  * @author norma
  */
 public class ConexionMySql {
-    
-    public static String url = "jdbc:mysql://localhost/proyecto1";
-    public static String usuario = "sa";
-    public static String clave = "password";
-    public static String clase = "com.mysql.jdbc.Driver";
-    PreparedStatement ps = null;
-    
-    public static Connection conectar() {
-        Connection conexion = null;
-        try {
-            Class.forName(clase);
-            conexion = (Connection) DriverManager.getConnection(url, usuario, clave);
-            System.out.println("Conexion establecida..");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e);
-        }
-        return conexion;
-    }
-    
-    
-    
-    
-    public ResultSet ejecutarConsulta(String sql) throws Exception {
-        Statement st = null;
-        st = conectar().createStatement();
-        ResultSet rs = st.executeQuery(sql);
-        return rs;
-    }
-    
-    
-    
-    
-    public int ejecutarActualizacionP(String sql, Object[] params) throws SQLException {
-        ps = conectar().prepareStatement(sql);
-        //definir los valores para los argumentos
-        for (int i = 0; i < params.length; i++) {
-            ps.setObject(i + 1, params[i]);
-        }
-        int r = ps.executeUpdate(); //retorna la cantidad de registros actualizados    
-        return r;
-    }
-    
-    
-    public void desconectar() throws SQLException {
-        conectar().close();
-    }
-
+ //esta imiplementación no jalaba, hay que adaptar el conector de LoginDAO para que la conexión se haga qui
+ //y el querry allá
     
 }
